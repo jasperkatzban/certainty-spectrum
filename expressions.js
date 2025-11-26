77ce35a693178296e13534c3d5c2903441ef9e2e
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2025-11-26 13:19:30 +0100

    draft of expressions

--- expressions.js
+++ expressions.js
@@ -1,6 +1,18 @@
 // expressions, on the spectrum from hopeless to unknowing to hopeful
 const expressions = [
-    "why even try",
-    "I simply do not know what will happen",
-    "it will all be fine",
+    "I'm too small to make any meaningful difference",
+    "we are doomed to self destruct because it is in our nature to do so",
+    "the trophies won through violent extraction are simply too addictive to ween from",
+    "we live in the ongoing results of centuries of imperialism, which will take many years to transcend",
+    "the momentum of the current system ensures that any substantial shift from business as usual will be violent",
+    "it's easier to conform to the system than to fight it, even though it does not produce conditions conducive to life",
+    "a break from the dominant system will not happen all at once nor does it need to in order to be valuable",
+    "I simply do not know what will happen and predicting the future with any certainty is a myth",
+    "the spaces left by the collapse of oppressive institutions are ripe with opportunity for collective thriving",
+    "our collective access to unfathomably vast amounts information makes it easier to learn from our mistakes",
+    "collective movements back to right relationships with earth are the only viable action",
+    "thanks to the internet, it has never been easier to learn about liberatory means",
+    "governments will be run on discord and democratic technology is right around the corner",
+    "people will always persevere, and it's best to stay present and do what you can from where you are",
+    "I'm being an alarmist; things will probably be alright",
 ]
